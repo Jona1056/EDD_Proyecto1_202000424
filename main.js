1d64ad7ffbbd0cb58d9105e3b776fd7b33cd3885
Jona1056 <3685713940101@ingenieria.usac.edu.gt>
2022-12-03 00:42:08 -0600

    lista enlazada y pantalla principal

---
+++ main.js
@@ -0,0 +1,37 @@
+class Cliente{
+    constructor(dpi,name,username,password,phone,admin,next){
+        this.dpi = dpi;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.admin = admin;
+        this.next = next;
+    };
+};
+
+class listaenlazada{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    };
+    add(dpi1,name1,username1,password1,phone1,admin1){
+        const Nodo = new Cliente(dpi1,name1,username1,password1,phone1,admin1, null);
+        if (!this.head){
+            this.head = Nodo
+        }else{
+            let registro = this.head;
+            while(registro.next){
+                registro = registro.next;
+            };
+            registro.next = Nodo;
+
+        };
+        this.size++;
+    };
+    
+}
+
+let list = new listaenlazada();
+list.add(123123,"Jonatan","jona",1234,123123123,true);
+list.add(1231212413,"andres","andres20",112334,12331,false);
